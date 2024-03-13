2a5ef453f69cbb5e2cd050dde19bb9c20b49c47a
gooseatTOKYO <hiromi.karizumi.0418@gmail.com>
2024-03-13 23:00:05 +0900

    課題途中経過

---
+++ src/main/java/com/techacademy/controller/ReportsController.java
@@ -0,0 +1,142 @@
+package com.techacademy.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.techacademy.constants.ErrorKinds;
+import com.techacademy.constants.ErrorMessage;
+
+import com.techacademy.entity.Report;
+import com.techacademy.service.ReportService;
+
+import com.techacademy.service.UserDetail;
+
+@Controller
+@RequestMapping("reports")
+public class ReportsController {
+
+    private final ReportService reportService;
+
+    @Autowired
+    public ReportsController(ReportService reportService) {
+        this.reportService = reportService;
+    }
+
+    // 日報一覧画面
+    @GetMapping
+    public String list(Model model) {
+
+        model.addAttribute("listSize", reportService.findAll().size());
+        model.addAttribute("reportList", reportService.findAll());
+
+        return "reports/list";
+    }
+//
+//    // 日報詳細画面
+//    @GetMapping(value = "/{code}/")
+//    public String detail(@PathVariable String code, Model model) {
+//
+//        model.addAttribute("report", reportService.findByCode(code));
+//        return "reports/detail";
+//    }
+//
+//    // 日報新規登録画面
+//    @GetMapping(value = "/add")
+//    public String create(@ModelAttribute Report report) {
+//
+//        return "reports/new";
+//    }
+//
+//    // 日報新規登録処理
+//    @PostMapping(value = "/add")
+//    public String add(@Validated Report report, BindingResult res, Model model) {
+//
+//        // パスワード空白チェック
+//        if ("".equals(report.getPassword())) {
+//            // パスワードが空白だった場合
+//            model.addAttribute(ErrorMessage.getErrorName(ErrorKinds.BLANK_ERROR),
+//                    ErrorMessage.getErrorValue(ErrorKinds.BLANK_ERROR));
+//
+//            return create(report);
+//
+//        }
+//
+//        // 入力チェック
+//        if (res.hasErrors()) {
+//            return create(report);
+//        }
+//
+//        // 論理削除を行った従業員番号を指定すると例外となるためtry~catchで対応
+//        // (findByIdでは削除フラグがTRUEのデータが取得出来ないため)
+//        try {
+//            ErrorKinds result = reportService.save(report);
+//
+//            if (ErrorMessage.contains(result)) {
+//                model.addAttribute(ErrorMessage.getErrorName(result), ErrorMessage.getErrorValue(result));
+//                return create(report);
+//            }
+//
+//        } catch (DataIntegrityViolationException e) {
+//            model.addAttribute(ErrorMessage.getErrorName(ErrorKinds.DUPLICATE_EXCEPTION_ERROR),
+//                    ErrorMessage.getErrorValue(ErrorKinds.DUPLICATE_EXCEPTION_ERROR));
+//            return create(report);
+//        }
+//
+//        return "redirect:/reports";
+//    }
+//
+//    //　日報削除処理
+//    @PostMapping(value = "/{code}/delete")
+//    public String delete(@PathVariable String code, @AuthenticationPrincipal UserDetail userDetail, Model model) {
+//
+//        ErrorKinds result = reportService.delete(code, userDetail);
+//
+//        if (ErrorMessage.contains(result)) {
+//            model.addAttribute(ErrorMessage.getErrorName(result), ErrorMessage.getErrorValue(result));
+//            model.addAttribute("report", reportService.findByCode(code));
+//            return detail(code, model);
+//        }
+//
+//        return "redirect:/reports";
+//    }
+//
+//    // 日報更新画面
+//    @GetMapping(value = "/{code}/update")
+//    public String edit(@PathVariable String code,Model model) {
+//        model.addAttribute("reports", reportService.findByCode(code));
+//        return "reports/update";
+//    }
+//
+// // 日報更新登録処理
+//    @PostMapping(value = "/{code}/update")
+//    public String update(@PathVariable String code,@Validated Report report, BindingResult res, Model model) {
+//
+//        // 入力チェック
+//         if (res.hasErrors()) {
+//             //ErrorMessage.getErrorValue(ErrorKinds.DUPLICATE_EXCEPTION_ERROR);
+//             return "reports/update";
+//         }
+//         ErrorKinds result = reportService.update(report);
+//
+//         if (ErrorMessage.contains(result)) {
+//              model.addAttribute(ErrorMessage.getErrorName(result), ErrorMessage.getErrorValue(result));
+//              ErrorMessage.getErrorValue(ErrorKinds.DUPLICATE_EXCEPTION_ERROR);
+//
+//              return "reports/update";
+//          }
+//        return "redirect:/reports";
+//    }
+
+}
+
---
+++ src/main/java/com/techacademy/service/ReportService.java
@@ -0,0 +1,44 @@
+package com.techacademy.service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import com.techacademy.constants.ErrorKinds;
+import com.techacademy.entity.Report;
+import com.techacademy.repository.ReportRepository;
+
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class ReportService {
+
+    private final ReportRepository reportRepository;
+
+    @Autowired
+    public ReportService(ReportRepository reportRepository, PasswordEncoder passwordEncoder) {
+        this.reportRepository = reportRepository;
+    }
+
+
+
+    // 従業員一覧表示処理
+    public List<Report> findAll() {
+        return reportRepository.findAll();
+    }
+
+    // 1件を検索
+    public Report findById(Integer id) {
+        // findByIdで検索
+        Optional<Report> option = reportRepository.findById(id);
+        // 取得できなかった場合はnullを返す
+        Report report = option.orElse(null);
+        return report;
+    }
+}
